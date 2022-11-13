b900c227e85a2683898cce04a3da6ce675ed45f5
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-11-13 12:49:35 -0500

    commit tree c659b2b1937582146d3d15131f080ed7afd2cace

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"]
+function writeCards (names, event){
+const messages = []
+for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+console.log(messages)
+return messages;
+}
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number = number - 1
+    }
+}
